470558760c62e4a003b04818acc1105aa70e89bf
Rodrigo <rodrigobriones54@gmail.com>
2021-08-09 17:36:13 +0000

    upload file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,5 +2,5 @@
 CREATE SCHEMA IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT ALL ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
