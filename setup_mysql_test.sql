46df32ed240f13fe020b20c164f3874f2db307cc
Rodrigo <rodrigobriones54@gmail.com>
2021-08-09 17:39:19 +0000

    upload file

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- script that prepares a MySQL server for the project
+CREATE SCHEMA IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
