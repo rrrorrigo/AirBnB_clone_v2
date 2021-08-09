1deb6f31aedd20c4e47e36ef4f89a8cf24a8e55e
Rodrigo <rodrigobriones54@gmail.com>
2021-08-09 19:06:50 +0000

    fixed documentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage:
@@ -42,6 +35,14 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -57,5 +58,6 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
+        """Delete dictionary of object storage"""
         if obj:
             self.__objects.pop(obj.__class__.__name__ + "." + obj.id)
