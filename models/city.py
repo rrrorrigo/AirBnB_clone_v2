bb2c15725d353dfc214eda8686923fbe73c07b06
rrrorrigo <rodrigobriones54@gmail.com>
2021-08-10 19:59:20 -0300

    added relationship

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
@@ -9,3 +10,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    state = relationship("state", cascade="all, delete", backref="cities")
