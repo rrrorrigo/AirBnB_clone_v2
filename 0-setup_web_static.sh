e32ea54e382ce4690b19d848e69c4a0e227c511b
rrrorrigo <rodrigobriones54@gmail.com>
2021-08-26 19:41:51 +0000

    erase current directory creation

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,6 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/current/
 sudo echo "<html>
   <head>
   </head>
@@ -12,9 +11,8 @@ sudo echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-cd /data/web_static
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "/listen 80 default_server/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
