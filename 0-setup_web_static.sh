32924f639332438e7b6ea7a28e7c5838a628910e
rrrorrigo <rodrigobriones54@gmail.com>
2021-08-26 19:29:37 +0000

    fixed sed error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,10 +15,6 @@ sudo echo "<html>
 cd /data/web_static
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-sudo sed -i "/listen 80 default_server/a location /hbnb_static {
-alias /data/web_static/current/;
-error_page  404  /404.html;
-try_files $uri $uri/ =404;
-}" /etc/nginx/sites-available/default
+sudo sed -i "/listen 80 default_server/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
