3ecfa364734cb907151d59b3dd3c8a9d60dad498
rrrorrigo <rodrigobriones54@gmail.com>
2021-08-26 13:32:14 +0000

    bash script to setup web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+sudo apt install -y nginx
+mkdir data && cd data
+mkdir web_static && cd web_static
+mkdir releases shared && cd releases
+mkdir test && cd test
+echo "test text" > index.html
+cd /data/web_static
+mkdir current
+if [ -L 'current' ]
+then
+    unlink current
+    mkdir current
+fi
+ln -s current /data/web_static/releases/test/
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+sed -i "$d" /etc/nginx/sites-available/default
+echo "location /hbnb_static {
+alias /data/web_static/current/;
+error_page  404  /404.html;
+try_files $uri $uri/ =404;
+}
+}" >> /etc/nginx/sites-available/default
