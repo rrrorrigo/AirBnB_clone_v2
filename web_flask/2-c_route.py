ff9250240f2409e8744301ed886b25a9838fd75a
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-11 19:24:29 +0000

    upload file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello():
+        """display on route '/' Hello HBNB!"""
+        return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+        """display on route '/hbnb' HBNB"""
+        return "HBNB"
+
+
+@app.route("/c/<text>")
+def C(text):
+        """ followed by the value of the text variable 
+        (replace underscore _ symbols with a space )C display """
+        text = text.replace('_', ' ')
+        return "C {}".format(text)
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port='5000')
