58f5c70388562da557dbed662bc408f342231749
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-11 18:44:13 +0000

    fixed documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 from flask import Flask
-app = Flask(__name__)
 
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 @app.route("/")
 def hello():
@@ -11,4 +13,3 @@ def hello():
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port='5000')
-        app.url_map.strict_slashes = False
