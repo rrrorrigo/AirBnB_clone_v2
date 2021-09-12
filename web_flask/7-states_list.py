68c480ac262eeac0813e42c7c2907cb3b6d8aadf
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-12 21:56:04 +0000

    upload files

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states_list")
+def stateList():
+    """display a HTML page: (inside the tag BODY)"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", state=states)
+
+
+@app.teardown_appcontext
+def remove():
+    """removes the current SQLAlchemy session"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
