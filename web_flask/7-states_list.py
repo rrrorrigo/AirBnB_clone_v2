034a631beee069aa47ee7a76f86d87a21ec7e714
rrrorrigo <rodrigobriones54@gmail.com>
2021-09-12 22:55:40 +0000

    fixed all

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,7 @@
 """script that starts a Flask web application"""
 
 from models import storage
+from models.state import State
 from flask import Flask
 from flask import render_template
 
@@ -12,8 +13,8 @@ app.url_map.strict_slashes = False
 @app.route("/states_list")
 def stateList():
     """display a HTML page: (inside the tag BODY)"""
-    states = storage.all("State")
-    return render_template("7-states_list.html", state=states)
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
